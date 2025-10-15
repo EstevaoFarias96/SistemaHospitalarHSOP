37c303283f43da31fb49301fc10a0640c0e5d17c
Estevao Farias <estfilho10@gmail.com>
2025-10-15 10:42:38 -0300

    ajuste prod

--- app/config.py
+++ app/config.py
@@ -30,7 +30,7 @@ class ConfigDev:
 
 # === ATIVAR UMA DAS CONFIGURAÇÕES ABAIXO ===
 # Config = ConfigProd  # Produção (Render, Railway, etc.)
-Config = ConfigDev # Desenvolvimento/teste local
+Config = ConfigProd # Desenvolvimento/teste local
 
 
 
